ad35cd974608e8cbb67885bf22c8aea68b9524ec
Colin Megill <colinmegill@gmail.com>
2013-12-01 22:55:32 -0800

    processing api call into vectors, halfway done

--- visualization.js
+++ visualization.js
@@ -1,24 +1,72 @@
-
 //http://api.nytimes.com/svc/search/v2/articlesearch.json?q=israel+iran&fq=source:("The New York Times")&api-key=f25c99da2f24daefca165f7a452d05ec:1:35029882
 
-
 var keywordsArray = []
+var templateVector = []
 
 for (ii=0; ii<10; ii++) {
-
 	$.ajax({
 		url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=israel+iran&fq=source:(%22The%20New%20York%20Times%22)&page="+ii+"&api-key=f25c99da2f24daefca165f7a452d05ec:1:35029882"
-	}).done(function(data){ 
-		data.response.docs.forEach(function(doc){
-			doc.keywords.forEach(function(keyword){
-				keywordsArray.push(keyword.value)
-			})
-		})
-		console.dir(data)
+	}).done(processDocs)
+}
+
+
+
+		
+
+function vectorizeStory (doc) {
+
+	var initialVector = []
+
+	doc.keywords.forEach(function(keyword){
+		
 	})
+
+	return vector;
 
 }
 
+function addToMasterKeywordsArray (doc) {
+	doc.keywords.forEach(function(keyword){
+		keywordsArray.push(keyword.value)
+	})
+}
+
+function createTemplateVectorMap (keywordsArray) {
+
+}
+
+function processDocs (data) {
+
+	//let's see what we get back...
+	console.dir(data)
+
+	//for each times story we get back... add each story's keywords to the master array
+	data.response.docs.forEach(addToMasterKeywordsArray)
+
+	//sort and unique, faster algo if sorted and we are sorting it, so pass true
+	//produce template vector
+	createTemplateVectorMap(_.uniq(keywordsArray.sort(), true))
+
+	//turn keyword list into vector ['iran', 'israel'] => [0, 1] etc.
+	data.response.docs.forEach(vectorizeStory)
+
+	
+
+}
+
+
+
+//DONE sort keywords array
+//DONE uniq sorted array
+// de facto at which position each keyword belongs... index 50 is 'iran'... each time take in a story... 
+// when want to make [1,0]... make an empty vector full of zeros... make a map out of it too... 
+// make an object that would be a map... keywords[currentWord] = indexposition
+// index of 1s and 0s... 
+// when training... pass in... array of vectors that am creating... training case is the classifcation value
+// when classifcation ... ... vector is the target vector... array of those 
+// transform vectors into format that they want - just so happens that the input and output are the same 
+// after i've trained it, go through them one at a time and check the hidden layer. 
+// that's the x y position!!!!!
 
 
 
@@ -36,7 +84,7 @@ var svg = d3.select("body")
 						.attr("border", "1px solid black")
 
 svg.selectAll("circle")
-		.data(dataset)
+		.data([1,2,3,10,40])
 		.enter()
 		.append("circle")
 		.attr("r", function(d, i){
