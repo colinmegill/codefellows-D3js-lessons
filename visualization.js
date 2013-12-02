32a84beecda2fc043c39fcb63a6af0d73f86c3a6
Colin Megill <colinmegill@gmail.com>
2013-12-02 00:28:41 -0800

    beginning work on neural nets, processing mostly done

--- visualization.js
+++ visualization.js
@@ -1,41 +1,60 @@
 //http://api.nytimes.com/svc/search/v2/articlesearch.json?q=israel+iran&fq=source:("The New York Times")&api-key=f25c99da2f24daefca165f7a452d05ec:1:35029882
 
-var keywordsArray = []
-var templateVector = []
+var storiesToRequest = 10;
 
-for (ii=0; ii<10; ii++) {
+for (ii=0; ii < storiesToRequest; ii++) {
 	$.ajax({
 		url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=israel+iran&fq=source:(%22The%20New%20York%20Times%22)&page="+ii+"&api-key=f25c99da2f24daefca165f7a452d05ec:1:35029882"
-	}).done(processDocs)
-}
-
-
-
-		
-
-function vectorizeStory (doc) {
-
-	var initialVector = []
-
-	doc.keywords.forEach(function(keyword){
-		
-	})
-
-	return vector;
-
+	}).done(initializeNeuralNetwork)
 }
 
 function addToMasterKeywordsArray (doc) {
+
+	console.log('adding keys to master list...')
+
 	doc.keywords.forEach(function(keyword){
 		keywordsArray.push(keyword.value)
 	})
+
+	console.log('the master list now has ' + keywordsArray.length + ' elements in it.')
+
 }
 
 function createTemplateVectorMap (keywordsArray) {
+	
+	console.log('creating templateVectorMap...')
+
+	_.each(keywordsArray, function(keyword, indexposition){
+		templateVectorMap[keyword] = indexposition;
+	})
+
+	console.dir('created templateVectorMap: ' + templateVectorMap)
 
 }
 
+function vectorizeStory (doc) {
+
+	//we push arrays onto the trainingData array
+	var vector = []
+
+	doc.keywords.forEach(function(keyword){
+	
+
+		trainingData.push(vector)
+	})
+
+
+}
+
+
+
+
 function processDocs (data) {
+
+	var keywordsArray = [];
+	var templateVectorMap = {};
+	var trainingData = [];
+
 
 	//let's see what we get back...
 	console.dir(data)
@@ -50,7 +69,25 @@ function processDocs (data) {
 	//turn keyword list into vector ['iran', 'israel'] => [0, 1] etc.
 	data.response.docs.forEach(vectorizeStory)
 
-	
+	return {
+		templateVectorMap: templateVectorMap,
+		trainingData: trainingData
+	}
+
+}
+
+
+function initializeNeuralNetwork (data) {
+
+	var nytimes = processDocs(data)
+	var neuralNetwork = new brain.NeuralNetwork({
+		hiddenLayers: [2]
+	})
+
+	neuralNetwork.train(nytimes.trainingData);
+
+	var coordinates = [] //array of arrays for d3 to scatterplot...
+	//must ask mike... believe neuralnetwork.js line 33
 
 }
 
@@ -60,8 +97,8 @@ function processDocs (data) {
 //DONE uniq sorted array
 // de facto at which position each keyword belongs... index 50 is 'iran'... each time take in a story... 
 // when want to make [1,0]... make an empty vector full of zeros... make a map out of it too... 
-// make an object that would be a map... keywords[currentWord] = indexposition
-// index of 1s and 0s... 
+//DONE make an object that would be a map... keywords[currentWord] = indexposition
+//DONE index of 1s and 0s... 
 // when training... pass in... array of vectors that am creating... training case is the classifcation value
 // when classifcation ... ... vector is the target vector... array of those 
 // transform vectors into format that they want - just so happens that the input and output are the same 
